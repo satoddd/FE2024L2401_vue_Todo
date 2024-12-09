25ac9a855d19f00d3dacbda35aa70bfc0d224d3b
satoddd <2401332@s.asojuku.ac.jp>
2024-12-09 11:43:52 +0900

    aa

--- Todo/script/script.js
+++ Todo/script/script.js
@@ -3,15 +3,29 @@ new Vue({
   data() {
     return {
       //データの準備
-
+      todos: [],
+      text: ''
     };
   },
   methods: {
     //inputTextメソッドを追加
-
+    inputText(e) {
+      this.text = e.target.value;
+    },
     //addTodoメソッドを追加
-
+    addTodo(){
+      if (!this.text) return;
+      const text = this.text;
+      const id = Math.ceil(Math.random() * 100);
+      const todo = {
+        id, text, isDone:false
+      };
+      this.todos.push(todo);
+      this.resetText();
+    },
     //resetTextメソッドを追加
-    
+    resetText(){
+      this.text = '';
+    }
   }
 });
